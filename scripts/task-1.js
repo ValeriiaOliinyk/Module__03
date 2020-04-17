964c2b3be7c8ade3fcbcd18c31788989ae0c08ed
Valeriia Oliinyk <olijnykvaleria@gmail.com>
2020-04-17 23:04:31 +0300

    added first task

---
+++ scripts/task-1.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
+const keys = Object.keys(user);
+for (const item of keys) {
+  console.log(`${item} : ${user[item]}`);
+}
